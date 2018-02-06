ae78d9a9c8147f08ea4e96423e69bb601fb0c331
MikaElola <34126826+MikaElola@users.noreply.github.com>
2018-02-06 14:30:36 +0200

    Kirja repository lisatty ja sinne pari testi kirjaa

--- BookStore/src/main/java/hh/BookStore/BookStore/BookStoreApplication.java
+++ BookStore/src/main/java/hh/BookStore/BookStore/BookStoreApplication.java
@@ -1,12 +1,38 @@
 package hh.BookStore.BookStore;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.BookStore.BookStore.domain.Book;
+import hh.BookStore.BookStore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookStoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookStoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+	return (args) -> {
+	 log.info("pari kirjaa talteen");
+	 repository.save(new Book("Cat in a Hat", "Dr.Seuss", 1950, 12345, 20.00));
+	 repository.save(new Book("1984", "George Orwell", 1946, 5678, 25.00));
+	
+	 log.info("hae kaikki kirjat");
+	 for (Book book : repository.findAll()) {
+		 log.info(book.toString());
+	 }
+	
+	};
+	}
+	
 }
--- BookStore/src/main/java/hh/BookStore/BookStore/domain/Book.java
+++ BookStore/src/main/java/hh/BookStore/BookStore/domain/Book.java
@@ -1,7 +1,16 @@
 package hh.BookStore.BookStore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 	public String title;
 	public String author;
 	public int year;
